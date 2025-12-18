a3e49c5c5c93c0458d3e06e24802e2625725ce51
bobojonov-sobirjon <sobirbobojonov05763104@gmail.com>
2025-12-18 22:02:02 +0500

    chnaged123

--- config/settings.py
+++ config/settings.py
@@ -80,12 +80,12 @@ AUTH_USER_MODEL = 'website.User'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
